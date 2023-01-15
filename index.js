f40bdf67cdf558ddf51cdf5e5de4f82c97d8d65a
Phanuel <ndikumanaphanuel@gmail.com>
2023-01-15 19:34:59 +0200

    ft(create):
    -sending message to the owner of the blog
    - deleting a message
    Finishes003

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const contactRoute = require("./routes/contacts");
 const multer = require("multer");
 
 dotenv.config();
@@ -29,6 +30,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
+app.use("/api/contacts",contactRoute);
 app.listen("5000", ()=>{
     console.log("Backend is running.");
 });
