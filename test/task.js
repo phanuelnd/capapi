85d41c1c1913826c63e1e5a0d37e558024b6587d
Phanuel <ndikumanaphanuel@gmail.com>
2023-01-20 13:56:50 +0200

    ft(Test):
    - Testing different routes
    - Hiding my credentials
    Finishes002

--- test/task.js
+++ test/task.js
@@ -10,9 +10,9 @@ chai.use(chaiHttp);
 describe('Blogs API', () =>{
     //test Post
     describe("POST /api/posts", () => {
-        it("It should Post all the tasks", (done) =>{
+        it("It should Post all the information of blog", (done) =>{
             chai.request(app)
-            .get("/api/posts/")
+            .post("/api/posts/")
             .end((err, response) => {
                 response.should.have.status(200);
                 done();
@@ -22,7 +22,7 @@ describe('Blogs API', () =>{
 
 
     describe("PUT /api/posts", () => {
-        it("It should Update all the data", (done) =>{
+        it("It should Update all the data of the blog id", (done) =>{
             chai.request(app)
             .put("/api/posts/id")
             .end((err, response) => {
@@ -33,9 +33,55 @@ describe('Blogs API', () =>{
     })
 
     describe("PUT /api/posts", () => {
-        it("It should Update all the data", (done) =>{
+        it("It should Update all the data of the given blog", (done) =>{
             chai.request(app)
             .put("/api/posts/id")
+            .end((err, response) => {
+                response.should.have.status(200);
+                done();
+            })
+        })
+    })
+
+
+    describe("GET /api/posts", () => {
+        it("It should Get all the blogs.", (done) =>{
+            chai.request(app)
+            .get("/api/posts")
+            .end((err, response) => {
+                response.should.have.status(200);
+                done();
+            })
+        })
+    })
+
+
+    describe("DELETE /api/posts", () => {
+        it("It should delete the blog post that match provided ID", (done) =>{
+            chai.request(app)
+            .delete("/api/posts/id")
+            .end((err, response) => {
+                response.should.have.status(200);
+                done();
+            })
+        })
+    })
+
+    describe("POST /api/contacts", () => {
+        it("It should Send any message.", (done) =>{
+            chai.request(app)
+            .post("/api/contacts")
+            .end((err, response) => {
+                response.should.have.status(200);
+                done();
+            })
+        })
+    })
+
+    describe("GET /api/contacts", () => {
+        it("It should Get any message.", (done) =>{
+            chai.request(app)
+            .get("/api/contacts")
             .end((err, response) => {
                 response.should.have.status(200);
                 done();
